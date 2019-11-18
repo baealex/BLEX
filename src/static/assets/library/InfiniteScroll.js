66e7cfaa34ab016926959ba0911ffc9ab20f177e
baealex <im@baejino.com>
2019-11-18 18:08:49 +0900

    [style] update

--- src/static/assets/library/InfiniteScroll.js
+++ src/static/assets/library/InfiniteScroll.js
@@ -49,3 +49,15 @@ class InfiniteScroll {
         });
     }
 }
+
+$(document).ready(function() {
+    var desPos = getCookie('latelyposition');
+    var findLatelyPosition = setInterval(function() {
+      var nowPos = $(document).scrollTop();
+      if(nowPos != desPos) {
+        $(document).scrollTop(desPos);
+      } else {
+        clearTimeout(findLatelyPosition);
+      }
+    }, 100);
+});
