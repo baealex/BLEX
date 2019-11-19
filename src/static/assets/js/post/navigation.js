e7de82cc84ff144722343e3249a6949061dcb99c
baealex <im@baejino.com>
2019-11-19 11:46:06 +0900

    [fix] article nav

--- src/static/assets/js/post/navigation.js
+++ src/static/assets/js/post/navigation.js
@@ -1,24 +1,24 @@
-/* Side navigation */
-var navcounter = 0;
-var isCreateNav = false;
-var items = $.makeArray($('.article h1,.article h2,.article h3,.article h4,.article h5,.article h6').map(function () {
-    navcounter += 1;
-    isCreateNav = true;
-    var makeTag = document.createElement('a');
-    makeTag.appendChild(document.createTextNode($(this).text()));
-    makeTag.setAttribute('id', 'nav' + navcounter);
-    makeTag.href = 'javascript:void(0)';
-    makeTag.setAttribute('onclick', 'moveSlide(' + navcounter + ')');
-    $('#article-nav').append(makeTag);
-    return $(this).attr('id', navcounter );
-}));
-
-/* Top navigation */
 $(document).ready(function () {
-    screenHeight = $(window).height();
-    screenHeightWithoutTopnav = $(window).height() - 100;
+    /* CREATE ARTICLE NAVIGATION */
+    var navcounter = 0;
+    var isCreateNav = false;
+    var items = $.makeArray($('.article h1,.article h2,.article h3,.article h4,.article h5,.article h6').map(function () {
+        navcounter += 1;
+        isCreateNav = true;
+        var makeTag = document.createElement('a');
+        makeTag.appendChild(document.createTextNode($(this).text()));
+        makeTag.setAttribute('id', 'nav' + navcounter);
+        makeTag.href = 'javascript:void(0)';
+        makeTag.setAttribute('onclick', 'moveSlide(' + '\'headline' + navcounter + '\')');
+        $('#article-nav').append(makeTag);
+        return $(this).attr('id', 'headline' + navcounter );
+    }));
+
+    var screenHeight = $(window).height();
+
     $(window).scroll(function () {
-        if ($(this).scrollTop() < screenHeight) {
+        screenPosition = $(this).scrollTop();
+        if (screenPosition < screenHeight) {
             $('#mNavigation').css("position", "absolute");
             $('#mNavigation').css("background", "rgba(0,0,0,0)");
             $("#mNavigation").removeClass("slide_bottom");
@@ -28,16 +28,16 @@ $(document).ready(function () {
             $("#mNavigation").addClass("slide_bottom");
         }
         if (isCreateNav) {
-            screenPosition = $(document).scrollTop();
-            for(let j=1; j<navcounter; j++) {
-                if(screenHeightWithoutTopnav-screenPosition > $(`#${j}`).position().top && screenHeightWithoutTopnav-screenPosition < $(`#${j+1}`).position().top) {
-                    $(`#nav${j}`).addClass('nav-now');
+            var btween = screenPosition - (screenHeight - 100);
+            for(let i=1; i<navcounter; i++) {
+                if(btween > $(`#headline${i}`).position().top && btween < $(`#headline${i+1}`).position().top) {
+                    $(`#nav${i}`).addClass('nav-now');
                 }
                 else {
-                    $(`#nav${j}`).removeClass('nav-now');
+                    $(`#nav${i}`).removeClass('nav-now');
                 }
             }
-            if(screenHeightWithoutTopnav-screenPosition > $(`#${navcounter}`).position().top) {
+            if(btween > $(`#headline${navcounter}`).position().top) {
                 $(`#nav${navcounter}`).addClass('nav-now');
             }
             else {
