b84772bdc27184c99ba11c67c65d8286eb3e1f13
Jino Bae <im@baejino.com>
2019-12-02 15:03:36 +0000

    [fix] append setting oops template

--- src/main/settings_1.py
+++ src/main/settings_1.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 'oops' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
