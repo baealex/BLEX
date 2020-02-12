80a99bdaf2b39cb900ee9a2f0e3c0ed9758e2ad6
Jino Bae <im@baejino.com>
2020-02-12 14:01:39 +0000

    [feat] story image upload

--- src/static/assets/js/thread.js
+++ src/static/assets/js/thread.js
@@ -8,13 +8,13 @@ var story = {
             $('#story-form input').val('');
             $('#story-form textarea').val('');
             $('#story').prepend(`<div id="story-${data.element.pk}">${render.story(data.element)}</div>`);
-        });    
+        });
     },
     edit: (pk) => {
         $.ajax({
             type: "GET",
             url: `/api/v1/story/${pk}?get=form`,
-        }).done(function(data) {
+        }).done(function (data) {
             $(`#story-${pk}`).html(data);
         });
     },
@@ -22,7 +22,7 @@ var story = {
         $.ajax({
             type: "GET",
             url: `/api/v1/story/${pk}`,
-        }).done(function(data) {
+        }).done(function (data) {
             $(`#story-${pk}`).html(render.story(data.element));
         });
     },
@@ -31,19 +31,19 @@ var story = {
             type: "PUT",
             url: `/api/v1/story/${pk}`,
             data: $(`#story-${pk}-form`).serialize(),
-        }).done(function(data) {
-            if(data.state == 'true') {
+        }).done(function (data) {
+            if (data.state == 'true') {
                 $(`#story-${pk}`).html(render.story(data.element));
             }
         });
     },
     remove: (pk) => {
-        if(confirm('정말 스토리를 삭제합니까?')) {
+        if (confirm('정말 스토리를 삭제합니까?')) {
             $.ajax({
                 url: `/api/v1/story/${pk}`,
                 type: "DELETE",
             }).done(function (data) {
-                if(data == 'DONE') {
+                if (data == 'DONE') {
                     $(`#story-${pk}`).remove();
                 }
             });
@@ -66,15 +66,38 @@ var thread = {
         }
     },
     remove: (pk) => {
-        if(confirm('정말 스레드를 삭제합니까?')) {
+        if (confirm('정말 스레드를 삭제합니까?')) {
             $.ajax({
                 url: `/api/v1/thread/${pk}`,
                 type: "DELETE",
             }).done(function (data) {
-                if(data == 'DONE') {
-                    location.href='/';
+                if (data == 'DONE') {
+                    location.href = '/';
                 }
             });
         }
     }
+}
+$('#image-upload-button').click(function (e) {
+    e.preventDefault();
+    $('#image-form > input').click();
+});
+function imageUpload() {
+    var imageForm = document.getElementById('image-form');
+    var formData = new FormData(imageForm);
+    $.ajax({
+        url: "/upload/image",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        cache: false,
+        processData: false,
+    }).done(function (data) {
+        $('#story-form textarea').val($('#story-form textarea').val() + `\n![](${data})\n`);
+        appendToast('이미지가 업로드 되었습니다.');
+    }).fail(function () {
+        appendToast('이미지 업로드에 실패했습니다.');
+    });
+    $('#image-form > input').val('');
+    appendToast('이미지를 업로드하는 중입니다.');
 }
