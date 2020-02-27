83ae6d7093c23f3e98c9234ef7b80017f3e14eed
Jino Bae <im@baejino.com>
2020-02-28 00:26:13 +0900

    [feat] update daily worker

--- src/util/daily_work.py
+++ src/util/daily_work.py
@@ -10,12 +10,33 @@ sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 django.setup()
 
-from board.models import Post, Thread
+from board.models import Post, Thread, Config
+
+send_user_names = tuple()
+send_user_lists = dict()
+for config in Config.objects.all():
+    if not config.telegram_id == '':
+        username = config.user.username
+        send_user_names += (username,)
+        send_user_lists[username] = {
+            'id': config.telegram_id,
+            'data': {
+                'total': 0,
+                'top': None,
+                'top_count': 0,
+            }
+        }
 
 posts = Post.objects.all()
 thread = Thread.objects.all()
 
 for post in posts:
+    username = post.author.username
+    if username in send_user_names:
+        send_user_lists[username]['data']['total'] += post.today
+        if post.today > send_user_lists[username]['data']['top_count']:
+            send_user_lists[username]['data']['top_count'] = post.today
+            send_user_lists[username]['data']['top'] = post.title
     if post.trendy > 0:
         post.trendy -= 1
     post.trendy += int(post.today/10)
@@ -33,7 +54,6 @@ for data in thread:
 print('ALL POSTS DONE!')
 
 from board.models import Profile
-
 profiles = Profile.objects.all()
 
 for data in profiles:
@@ -41,3 +61,20 @@ for data in profiles:
         data.exp -= 5
         data.save()
 print('ALL PROFILES DONE!')
+
+import random
+from board.telegram import TelegramBot
+from board.telegram_token import BOT_TOKEN
+bot = TelegramBot(BOT_TOKEN)
+for username in send_user_names:
+    total = send_user_lists[username]['data']['total']
+    top_posts = send_user_lists[username]['data']['top']
+    text = ''
+    if total:
+        text += '오늘의 총 조회수 : ' + str(total) + '\n'
+    if top_posts:
+        text += '오늘 가장 많이 조회된 글 : ' + top_posts + '\n'
+    emoji = ['😀', '😁', '😙', '🤗', '😏', '😥', '🥱', '😪', '😗', '😆', '🥰']
+    text += '오늘 하루도 고생하셨습니다 ' + emoji[random.randint(0, 10)]
+    bot.send_message_async(send_user_lists[username]['id'], text)
+print('ALL MESSAGE SEND!')
