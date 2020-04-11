1afc27c1b4bc9fcb2b443e9ac6a8eaa1439b238f
Jino Bae <im@baejino.com>
2020-04-11 23:24:04 +0900

    [feat] new markdown

--- src/api/parsedown/get.php
+++ src/api/parsedown/get.php
@@ -11,15 +11,17 @@
 
     // Custom Markdown
     $result = preg_replace(
-        '/@gif\[.*\"(https?:\/\/.*\.mp4)\".*\]/',
+        '/<p>@gif\[.*\"(https?:\/\/.*\.mp4)\".*\]<\/p>/',
         '<video autoplay muted loop><source src="$1" type="video/mp4"/></video>',
         $result
     );
     $result = preg_replace(
-        '/@youtube\[(.*)\]/',
+        '/<p>@youtube\[(.*)\]<\/p>/',
         '<iframe width="100%" height="315" src="https://www.youtube.com/embed/$1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
         $result
     );
+    $result = preg_replace('/>\[\s\]\s/', '><input type="checkbox" disabled> ', $result);
+    $result = preg_replace('/>\[x\]\s/', '><input type="checkbox" disabled checked> ', $result);
 
     // Allow Markup
     $result = preg_replace('/&lt;br\/?&gt;/', '<br>', $result);
