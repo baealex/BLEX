ccb43e9d531d7ffc623b4df4f90546ba00f5e8c9
baealex <im@baejino.com>
2020-08-10 11:24:46 +0900

    [feat] inner markdown parser : blexer

--- server/src/board/templatetags/util.py
+++ server/src/board/templatetags/util.py
@@ -1,6 +1,8 @@
 from django import template
 from django.utils.html import strip_tags
 
+from board.module.blexer import Blexer
+
 register = template.Library()
 
 def read_time(value):
@@ -9,5 +11,9 @@ def read_time(value):
 def get_type(element):
     return str(type(element))
 
+def blexer(text):
+    return Blexer().to_html(text)
+
 register.filter('read_time', read_time)
 register.filter('get_type', get_type)
+register.filter('blexer', blexer)
