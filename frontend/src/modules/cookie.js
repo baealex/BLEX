d60450b036252b9a3f87c729b9c08d88afa9067c
Jino Bae <im@baejino.com>
2020-09-08 03:57:00 +0000

    [style] change name parameter in cookie module

--- frontend/src/modules/cookie.js
+++ frontend/src/modules/cookie.js
@@ -1,16 +1,16 @@
 class Cookie {
-    set(name, value, info) {
-        let willCookie = name + '=' + value + ';';
-        if (info.expire) {
+    set(name, value, option) {
+        let willCookie = `${name}=${value};`;
+        if(option.expire) {
             let date = new Date();
-            date.setTime(date.getTime() + (info.expire * 24 * 60 * 60 * 1000));
-            willCookie += 'Expires=' + date.toUTCString() + ';';
+            date.setTime(date.getTime() + (option.expire * 24 * 60 * 60 * 1000));
+            willCookie += `Expires=${date.toUTCString()};`;
         }
-        if (info.domain) {
-            willCookie += 'Domain=' + info.domain + ';';
+        if(option.domain) {
+            willCookie += `Domain=${option.domain};`;
         }
-        if (info.path) {
-            willCookie += 'Path=' + info.path + ';';
+        if(option.path) {
+            willCookie += `Path=${option.path};`;
         }
         document.cookie = willCookie;
     }
