7ad4fdb9c94ace7cd204b4d2e6026165f2c31f97
baealex <im@baejino.com>
2020-10-27 11:42:58 +0900

    [feat] ga v4

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,8 +1,9 @@
 import App from 'next/app'
 import Head from 'next/head'
-import styles from '../styles/main.scss'
-
 import Router from 'next/router';
+
+import styles from '../styles/main.scss'
+import Config from '../modules/config.json'
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -41,6 +42,14 @@ class Main extends App {
                         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
                         crossOrigin="anonymous"
                     />
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VD3ZLTR4ZQ"></script>
+                    <script dangerouslySetInnerHTML={{ __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', '${Config.GOOGLE_ANALYTICS_V4}');
+                    `}}/>
                 </Head>
 
                 <TopNavagation/>
