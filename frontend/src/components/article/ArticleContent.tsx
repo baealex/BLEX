ed00903b71cf5910d5c039079f7599aef10b1311
baealex <im@baejino.com>
2020-12-16 19:57:15 +0900

    🛠 Apply route internal link

--- frontend/src/components/article/ArticleContent.tsx
+++ frontend/src/components/article/ArticleContent.tsx
@@ -1,6 +1,20 @@
+import Router from 'next/router';
+
+function onInnerLinkClickEvent(this: HTMLAnchorElement, e: any) {
+    if(this.href.includes(`${location.protocol}//${location.host}/`)) {
+        e.preventDefault();
+        Router.push(this.href).then(() => window.scrollTo(0, 0));
+    }
+}
+
 export default function ArticleContent(props: {
     html: string;
 }) {
+    if(typeof window !== "undefined") {
+        document.querySelectorAll('.article a').forEach(element => {
+            element.addEventListener('click', onInnerLinkClickEvent);
+        });
+    }
     return (
         <div
             className="article noto"
