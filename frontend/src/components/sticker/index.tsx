c152e53c6e3a821e6a9526504b77febcc0cd5dae
baealex <im@baejino.com>
2021-01-21 20:50:49 +0900

    🎨 Upload fonts

--- frontend/src/components/sticker/index.tsx
+++ frontend/src/components/sticker/index.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 export default function Sticker(props: Props) {
-    const SVG_FILE = `https://static.blex.me/assets/images/undraw/${props.name}.svg`
+    const SVG_FILE = `https://static.blex.me/assets/sticker/${props.name}.svg`
     return (
         <>
             <img className="w-100" src={SVG_FILE}/>
