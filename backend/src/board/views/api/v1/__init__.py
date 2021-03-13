7a7da23e15322315bd2a7c3ab3a2400303973467
baealex <im@baejino.com>
2021-03-13 11:32:01 +0900

    ⭐ Increase temp posts limit

--- backend/src/board/views/api/v1/__init__.py
+++ backend/src/board/views/api/v1/__init__.py
@@ -882,7 +882,7 @@ def temp_posts(request):
 
     if request.method == 'POST':
         temps = TempPosts.objects.filter(author=request.user).count()
-        if temps >= 5:
+        if temps >= 20:
             return HttpResponse('error:OF')
         
         body = QueryDict(request.body)
