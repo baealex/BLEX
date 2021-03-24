b2d09dd8d0ab58412f547480bbe880a3a9603c47
baealex <im@baejino.com>
2021-03-24 16:20:40 +0900

    🎨 Update copyright date

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer(props: FooterProps) {
         <footer className={`page-footer font-small ${props.bgdark ? 'bg-rdark' : ''}`}>
             {props.children}
             <div className="footer-copyright text-center py-3">
-                Copyright 2020 &copy; <a href="https://baejino.com">BaeJino</a>.
+                Copyright 2021 &copy; <a href="https://baejino.com">BaeJino</a>.
             </div>
         </footer>
     );
