95245a15da89988c43a41edcf1bbc8159397f51e
baealex <im@baejino.com>
2021-06-20 17:13:37 +0900

    🔧 Close dropbown when out foucs

--- frontend/src/components/atoms/dropdown/index.tsx
+++ frontend/src/components/atoms/dropdown/index.tsx
@@ -2,7 +2,11 @@ import styles from './Dropdown.module.scss';
 import classNames from 'classnames/bind';
 const cn = classNames.bind(styles);
 
-import { useState } from 'react';
+import {
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
 
 export interface DropdownProps {
     button: JSX.Element;
@@ -13,26 +17,50 @@ export interface DropdownProps {
 }
 
 export function Dropdown(props: DropdownProps) {
+    const box = useRef<HTMLDivElement>(null);
+    const toggle = useRef<HTMLSpanElement>(null);
+
     const [ isOpen, setIsOpen ] = useState(false);
+
+    useEffect(() => {
+        const handleClick = (e: MouseEvent) => {
+            const path = e.composedPath && e.composedPath();
+
+            if (
+                !path.includes(box.current as EventTarget) &&
+                !path.includes(toggle.current as EventTarget)
+            ) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClick);
+        return () => {
+            document.removeEventListener('click', handleClick);
+        }
+    }, []);
+
     return (
         <>
-            <div onClick={() => setIsOpen(!isOpen)}>
-                <span className={cn('button')}>
+            <div onClick={() => setIsOpen((prevIsOpen) => !prevIsOpen)}>
+                <span ref={toggle} className={cn('button')}>
                     {props.button}
                 </span>
-                {isOpen && (
-                    <div className={cn('menu')}>
-                        <ul>
-                            {props.menus.map((menu, idx) => (
-                                <li key={idx} onClick={menu.onClick}>
-                                    <a>
-                                        {menu.name}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                <div ref={box}>
+                    {isOpen && (
+                        <div className={cn('menu')}>
+                            <ul>
+                                {props.menus.map((menu, idx) => (
+                                    <li key={idx} onClick={menu.onClick}>
+                                        <a>
+                                            {menu.name}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     )
