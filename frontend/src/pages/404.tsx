ced875d7a6f48dc0932620e6098665adda3388a3
baealex <im@baejino.com>
2021-08-17 22:36:57 +0900

    🔧 404 page height issue

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -35,6 +35,7 @@ export default function NotFound() {
             <style jsx>{`
                 :global(.content) {
                     margin-top: 0 !important;
+                    height: 100vh;
                 }
             `}</style>
         </>
