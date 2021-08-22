6556bfa12730e98b0ff22d67e6570677b5c71ae0
baealex <im@baejino.com>
2021-08-22 13:39:52 +0900

    🎨 Change code indent

--- frontend/src/components/atoms/dropdown/index.tsx
+++ frontend/src/components/atoms/dropdown/index.tsx
@@ -59,12 +59,12 @@ export function Dropdown(props: DropdownProps) {
                             <ul>
                                 {props.menus.map((menu, idx) => (
                                     <li key={idx} onClick={menu.onClick} className={cn({ disable: menu.disable })}>
-                                            <span>
-                                                {menu.name}
-                                            </span>
-                                            {menu.icon && (
-                                                <i className={menu.icon}/>
-                                            )}
+                                        <span>
+                                            {menu.name}
+                                        </span>
+                                        {menu.icon && (
+                                            <i className={menu.icon}/>
+                                        )}
                                     </li>
                                 ))}
                             </ul>
