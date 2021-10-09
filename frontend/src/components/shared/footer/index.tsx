4b0092e47c183ed9d807bdaa309f976caca63659
Jino Bae <im@baejino.com>
2021-10-09 20:23:06 +0900

    🔨 Change footer design

--- frontend/src/components/shared/footer/index.tsx
+++ frontend/src/components/shared/footer/index.tsx
@@ -2,8 +2,6 @@ import styles from './Footer.module.scss';
 import classNames from 'classnames/bind';
 const cn = classNames.bind(styles);
 
-import { useState, useEffect } from 'react';
-
 export interface FooterProps {
     bgdark?: boolean;
     children?: JSX.Element;
@@ -13,19 +11,6 @@ export function Footer({
     bgdark = false,
     children,
 }: FooterProps) {
-    const [ idx, setIdx ] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIdx((prevIdx) => {
-                if (prevIdx < 2) return prevIdx + 1;
-                return 0;
-            });
-        }, 6000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <footer className={cn('footer', { bgdark })}>
             {children && (
@@ -33,18 +18,38 @@ export function Footer({
                     {children}
                 </div>
             )}
-            <div className={cn('copy', 'text-center')}>
-                <span>블로그는 나를 표현한다. 나를 표현하는 블렉스.</span>
-                <span>블렉스는 <a href="https://github.com/baealex/BLEX">모든 소스가 공개된</a> 서비스입니다.</span>
-                <span>오류가 있나요? <a href="mailto:im@baejino.com">메일을 통해</a> 알려주세요!</span>
-                <style jsx>{`
-                    span {
-                        display: block;
-                        height: 30px;
-                        transition: transform 0.5s ease;
-                        transform: translateY(${`-${idx * 100}%`});
-                    }
-                `}</style>
+            <div className={cn('text-center')}>
+                <div className={cn('title')}>BLEX</div>
+                <div className={cn('sub-title')}>
+                    BLOG EXPRESS ME
+                </div>
+                <div className={cn('items')}>
+                    <div>
+                        <a target="_blank" href="https://www.notion.so/58e7e6ab9a2d441d8f824ef852e77059">
+                            Terms
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://www.notion.so/b3901e0837ec40e3983d16589314b59a">
+                            Guide
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://discord.gg/d4vCnB3CSr">
+                            Discord
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://github.com/baealex/BLEX">
+                            GitHub
+                        </a>
+                    </div>
+                    <div>
+                        <a href="mailto:im@baejino.com">
+                            Contact
+                        </a>
+                    </div>
+                </div>
             </div>
         </footer>
     );
