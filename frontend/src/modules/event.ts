4b3724e763b5ecc05e4709ce1a3646fb8b093afd
Jino Bae <im@baejino.com>
2021-12-24 16:31:45 +0900

    ⭐ Add event modules

--- frontend/src/modules/event.ts
+++ frontend/src/modules/event.ts
@@ -1,13 +1,48 @@
-export function optimizedEvent(func: () => void) {
+export function optimizedEvent(func: (e?: Event) => void) {
     let ticking = false;
 
-    return () => {
+    return (e?: Event) => {
         if (ticking) return;
         
         window.requestAnimationFrame(() => {
-            func();
+            func(e);
             ticking = false;
         });
         ticking = true;
+    }
+}
+
+export interface DebounceEventRunner {
+    (e?: Event): boolean;
+    clear(): void;
+}
+
+export function debounceEvent(func: (e?: Event) => void, timing: number): DebounceEventRunner {
+    let timer: NodeJS.Timeout;
+
+    const runner = function(e?: Event) {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => func(e), timing);
+        return true;
+    }
+
+    runner.clear = function() {
+        if (timer) clearTimeout(timer);
+    }
+
+    return runner;
+}
+
+export function throttleEvent(func: (e?: Event) => void, timing: number) {
+    let isReady = true;
+
+    return (e?: Event) => {
+        if (isReady) {
+            isReady = false;
+            func(e);
+            setTimeout(() => {
+                isReady = true;
+            }, timing);
+        }
     }
 }
