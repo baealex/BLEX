216931896b6cfbdeb68d56a678c29eac2dd78069
Jino Bae <im@baejino.com>
2022-03-11 16:08:41 +0900

    🔨 Update referrer policy

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -57,12 +57,13 @@ class Main extends App<AppProps> {
             <>
                 <Head>
                     <title>BLEX</title>
+                    <meta name="referrer" content="origin"/>
                 </Head>
 
                 <SEO
                     title="BLEX"
                     image="https://static.blex.me/assets/images/default-post.png"
-                    description="모든 글은 경험에서 나오고, 그 경험은 곧 당신이 됩니다. 당신의 경험을 빛나게 할 블로그."
+                    description="모든 글은 경험에서 나오고, 그 경험은 곧 당신이 됩니다. 당신의 경험을 빛나게 할 블로그"
                 />
 
                 {CONFIG.GOOGLE_ANALYTICS_V4 && (
