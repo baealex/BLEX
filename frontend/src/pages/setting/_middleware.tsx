5b3eb4dd060c202cdffd806c5953a5a6aba72f81
Jino Bae <im@baejino.com>
2022-03-11 16:09:22 +0900

    🔧 Fix next middleware redirect error

--- frontend/src/pages/setting/_middleware.tsx
+++ frontend/src/pages/setting/_middleware.tsx
@@ -4,7 +4,10 @@ export async function middleware(req: NextRequest) {
     const cookie = req.headers.get('Cookie');
     
     if (!cookie || !cookie.includes('sessionid')) {
-        return NextResponse.redirect('/');    
+        const url = req.nextUrl.clone();
+        url.search = '';
+        url.pathname = '/';
+        return NextResponse.redirect(url);
     }
 
     return NextResponse.next();
