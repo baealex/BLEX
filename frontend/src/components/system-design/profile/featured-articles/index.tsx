62a5241c4762e9a54f55f6be8a05a3672bd473b1
Jino Bae <im@baejino.com>
2022-03-22 14:14:01 +0900

    🔨 Change feature article title text

--- frontend/src/components/system-design/profile/featured-articles/index.tsx
+++ frontend/src/components/system-design/profile/featured-articles/index.tsx
@@ -1,5 +1,3 @@
-import { Alert } from '@design-system';
-
 import {
     ArticleCardSmall,
     ArticleCardSmallProps,
@@ -12,19 +10,17 @@ export interface FeaturedArticlesProps {
 export function FeaturedArticles(props: FeaturedArticlesProps) {
     return (
         <>
-            <div className="h5 font-weight-bold mt-5">
-                인기 컨텐츠
-            </div>
-            {props.articles.length > 0 ? (
-                <div className="row mt-1 mb-5">
-                    {props.articles.map((article, idx) => (
-                        <ArticleCardSmall key={idx} {...article}/>
-                    ))}
-                </div>
-            ) : (
-                <Alert className="mt-3">
-                    아직 작성한 포스트가 없습니다.
-                </Alert>
+            {props.articles.length > 0 && (
+                <>
+                    <div className="h5 font-weight-bold mt-5">
+                        추천 컨텐츠
+                    </div>
+                    <div className="row mt-1 mb-5">
+                        {props.articles.map((article, idx) => (
+                            <ArticleCardSmall key={idx} {...article}/>
+                        ))}
+                    </div>
+                </>
             )}
         </>
     )
