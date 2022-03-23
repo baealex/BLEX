6c958cc184b066e25b98b7f16f35f04ae67bb677
Jino Bae <im@baejino.com>
2022-03-23 16:54:45 +0900

    🔨 Change design new editor title

--- frontend/src/components/system-design/article-editor-new/editor-title/index.tsx
+++ frontend/src/components/system-design/article-editor-new/editor-title/index.tsx
@@ -7,15 +7,17 @@ export interface EditorTitleProps {
 
 export function EditorTitle(props: EditorTitleProps) {
     return (
-        <>
+        <div className={styles.layout}>
+            <button>
+                <i className="far fa-image"/> 표지 이미지
+            </button>
             <input
                 name="title"
-                className={styles.title}
                 placeholder="제목을 입력하세요."
                 maxLength={50}
                 value={props.value}
                 onChange={(e) => props.onChange(e)}
             />
-        </>
+        </div>
     );
 }
