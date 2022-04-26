d0b50019ad3211657deec3fdfcab15394c10f8fb
Jino Bae <im@baejino.com>
2022-04-26 21:51:46 +0900

    ⭐ Add get an posts graphql

--- backend/src/board/schema.py
+++ backend/src/board/schema.py
@@ -1,17 +1,32 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from board.models import Post
+from board.models import Post, User, Series
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+class SeriesType(DjangoObjectType):
+    class Meta:
+        model = Series
+        fields = ('id', 'name', 'url', 'created_date', 'updated_date')
 
 class PostType(DjangoObjectType):
     class Meta:
         model = Post
-        fields = ('id', 'title')
+        fields = ('id', 'title', 'url', 'author', 'series', 'read_time', 'tags', 'created_date', 'updated_date')
 
 class Query(graphene.ObjectType):
-    all_post = graphene.List(PostType)
+    posts = graphene.Field(PostType, id=graphene.String(required=True))
 
-    def resolve_all_post(root, info):
-        return Post.objects.all()
+    def resolve_posts(root, info, id):
+        return Post.objects.get(id=id)
+    
+    all_posts = graphene.List(PostType)
+
+    def resolve_all_posts(root, info):
+        return Post.objects.select_related('author').all()
 
 schema = graphene.Schema(query=Query)
