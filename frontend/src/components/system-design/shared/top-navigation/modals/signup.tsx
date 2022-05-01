481e49024e32e80bc48b047362aff51c3f37fc6f
Jino Bae <im@baejino.com>
2022-05-01 16:57:15 +0900

    🔧 Fix build error

--- frontend/src/components/system-design/shared/top-navigation/modals/signup.tsx
+++ frontend/src/components/system-design/shared/top-navigation/modals/signup.tsx
@@ -97,7 +97,7 @@ export class SignupModal extends React.Component<Props, State> {
     
     render() {
         return (
-            <Modal title='회원등록' isOpen={this.props.isOpen} onClose={() => this.props.onClose()}>
+            <Modal title="회원등록" isOpen={this.props.isOpen} onClose={() => this.props.onClose()}>
                 {this.state.isDone ? (
                     <div className="mx-auto bg-border-purple p-3 bg-light deep-dark">
                         {this.state.realname}님의 가입을 진심으로 환영합니다! 💜
