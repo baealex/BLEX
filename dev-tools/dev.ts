eed4ab030b843cf41cca91b3dc09df1b245edadc
Jino Bae <im@baejino.com>
2022-07-14 22:46:43 +0900

    📦 Add workflow for dev image build

--- dev-tools/dev.ts
+++ dev-tools/dev.ts
@@ -2,46 +2,54 @@ import { readFileSync, writeFileSync, copyFileSync, existsSync } from 'fs'
 import { resolve } from 'path'
 import { runScript } from './core'
 
-if (!existsSync(resolve('./backend/.env')))
-    copyFileSync(
-        resolve('./dev-tools/sample/backend/.env'),
-        resolve('./backend/.env')
-    )
+const createDockerDev = () => {
+    if (!existsSync(resolve('./backend/.env')))
+        copyFileSync(
+            resolve('./dev-tools/sample/backend/.env'),
+            resolve('./backend/.env')
+        )
 
-if (!existsSync(resolve('./backend/src/db.sqlite3')))
-    copyFileSync(
-        resolve('./dev-tools/sample/backend/db.sqlite3'),
-        resolve('./backend/src/db.sqlite3')
-    )
+    if (!existsSync(resolve('./backend/src/db.sqlite3')))
+        copyFileSync(
+            resolve('./dev-tools/sample/backend/db.sqlite3'),
+            resolve('./backend/src/db.sqlite3')
+        )
 
-const beDockerFile = readFileSync(resolve('./backend/Dockerfile')).toString()
+    const beDockerFile = readFileSync(resolve('./backend/Dockerfile')).toString()
 
-const beDevCommand = `
+    const beDevCommand = `
 ENTRYPOINT ["python", "manage.py"]
 CMD ["runserver", "0.0.0.0:9000"]
 `
 
-writeFileSync(
-    resolve('./backend/DockerfileDev'),
-    beDockerFile.split('ENTRYPOINT')[0] + beDevCommand
-)
-
-if (!existsSync(resolve('./frontend/.env')))
-    copyFileSync(
-        resolve('./dev-tools/sample/frontend/.env'),
-        resolve('./frontend/.env')
+    writeFileSync(
+        resolve('./backend/DockerfileDev'),
+        beDockerFile.split('ENTRYPOINT')[0] + beDevCommand
     )
 
-const feDockerFile = readFileSync(resolve('./frontend/Dockerfile')).toString()
+    if (!existsSync(resolve('./frontend/.env')))
+        copyFileSync(
+            resolve('./dev-tools/sample/frontend/.env'),
+            resolve('./frontend/.env')
+        )
 
-const feDevCommand = `
+    const feDockerFile = readFileSync(resolve('./frontend/Dockerfile')).toString()
+
+    const feDevCommand = `
 ENTRYPOINT ["npm", "run"]
 CMD ["dev"]
 `
 
-writeFileSync(
-    resolve('./frontend/DockerfileDev'),
-    feDockerFile.split('RUN npm run build')[0] + feDevCommand
-)
+    writeFileSync(
+        resolve('./frontend/DockerfileDev'),
+        feDockerFile.split('RUN npm run build')[0] + feDevCommand
+    )
+}
 
-runScript('development')
+if (process.env.GITHUB_ACTION) {
+    createDockerDev()
+}
+
+if (!process.env.GITHUB_ACTION) {
+    runScript('development')
+}
