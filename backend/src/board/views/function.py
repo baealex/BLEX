4967ddaee8ab358b090c284656e00a05acdaf8d4
Jino Bae <im@baejino.com>
2022-07-17 23:47:17 +0900

    🏭 Remove unused method

--- backend/src/board/views/function.py
+++ backend/src/board/views/function.py
@@ -7,11 +7,3 @@ def compere_user(req, res, give_404_if='none'):
     else:
         if not req == res:
             raise Http404
-
-def page_check(page, paginator):
-    try:
-        page = int(page)
-    except:
-        raise Http404
-    if not page or int(page) > paginator.num_pages or int(page) < 1:
-        raise Http404
