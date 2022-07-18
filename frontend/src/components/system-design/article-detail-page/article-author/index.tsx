3bd640c110c083686351a9fe511a836a2acadfb1
Jino Bae <im@baejino.com>
2022-07-18 20:13:13 +0900

    🏭 Update author card

--- frontend/src/components/system-design/article-detail-page/article-author/index.tsx
+++ frontend/src/components/system-design/article-detail-page/article-author/index.tsx
@@ -3,6 +3,8 @@ import styles from './ArticleAuthor.module.scss';
 
 import Link from 'next/link';
 
+import { Card } from '@components/design-system';
+
 export interface ArticleAuthorProps {
     profile: {
         username: string;
@@ -14,35 +16,37 @@ export interface ArticleAuthorProps {
 
 export function ArticleAuthor(props: ArticleAuthorProps) {
     return (
-        <div className="d-flex align-items-center mb-5">
-            <div>
-                <Link href="/[author]" as={`/@${props.profile.username}`}>
-                    <a>
-                        <img
-                            className={styles.image}
-                            src={props.profile.image}
-                            width="100"
-                            height="100"
-                        />
-                    </a>
-                </Link>
-            </div>
-            <div className={classNames(styles.info)}>
-                <div className={styles.username}>
-                    <Link href={`/@${props.profile.username}`}>
+        <Card hasBackground isRounded className="p-3 mb-5">
+            <div className="d-flex align-items-center">
+                <div>
+                    <Link href="/[author]" as={`/@${props.profile.username}`}>
                         <a>
-                            {props.profile.realname} (@{props.profile.username})
+                            <img
+                                className={styles.image}
+                                src={props.profile.image}
+                                width="100"
+                                height="100"
+                            />
                         </a>
                     </Link>
                 </div>
-                <div className={styles.bio}>
-                    <Link href={`/@${props.profile.username}/about`}>
-                        <a>
-                            {props.profile.bio}
-                        </a>
-                    </Link>
+                <div className={classNames(styles.info)}>
+                    <div className={styles.username}>
+                        <Link href={`/@${props.profile.username}`}>
+                            <a>
+                                {props.profile.realname} (@{props.profile.username})
+                            </a>
+                        </Link>
+                    </div>
+                    <div className={styles.bio}>
+                        <Link href={`/@${props.profile.username}/about`}>
+                            <a>
+                                {props.profile.bio}
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Card>
     );
 }
