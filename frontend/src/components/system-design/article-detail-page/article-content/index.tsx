1262ad7b849fc0967f35101ac6c317812f07659a
Jino Bae <im@baejino.com>
2022-11-29 01:01:36 +0900

    ✨ Update code convention

--- frontend/src/components/system-design/article-detail-page/article-content/index.tsx
+++ frontend/src/components/system-design/article-detail-page/article-content/index.tsx
@@ -2,50 +2,38 @@ import classNames from 'classnames/bind';
 import styles from './ArticleContent.module.scss';
 const cn = classNames.bind(styles);
 
+import React from 'react';
 import Router from 'next/router';
-import { useEffect } from 'react';
 
 export interface ArticleContentProps {
     html: string;
-    isEdit?: boolean;
-    noMargin?: boolean;
 }
 
 export function ArticleContent({
-    html,
-    isEdit,
-    noMargin
+    html
 }: ArticleContentProps) {
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const handleClickAnchorTag = (e: any) => {
-                if (e.target.nodeName === 'A') {
-                    const { href } = e.target;
-                    const isSameOrigin = href.includes(`${location.protocol}//${location.host}/`);
+    const handleClickContent = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target instanceof HTMLAnchorElement) {
+            const { href } = e.target as HTMLAnchorElement;
+            const isSameOrigin = href.includes(`${location.protocol}//${location.host}/`);
 
-                    if (!isEdit && isSameOrigin) {
-                        e.preventDefault();
-                        Router.push(href).then(() => window.scrollTo(0, 0));
-                        return;
-                    }
+            if (isSameOrigin) {
+                e.preventDefault();
+                Router.push(href).then(() => window.scrollTo(0, 0));
+                return;
+            }
 
-                    e.preventDefault();
-                    window.open(href, '_blank');
-                    return;
-                }
-            };
-
-            const $article = document.querySelector(`.${styles.article}`);
-
-            $article?.addEventListener('click', handleClickAnchorTag);
-
-            return () => {
-                $article?.removeEventListener('click', handleClickAnchorTag);
-            };
+            e.preventDefault();
+            window.open(href, '_blank');
+            return;
         }
-    }, []);
+    };
 
     return (
-        <div className={cn('article', { noMargin })} dangerouslySetInnerHTML={{ __html: html }} />
+        <div
+            className={cn('article')}
+            onClick={handleClickContent}
+            dangerouslySetInnerHTML={{ __html: html }}
+        />
     );
 }
