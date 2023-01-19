05e9520e00bd4a13b9b04bc9e7ea919c66b07f17
Jino Bae <im@baejino.com>
2023-01-20 01:57:44 +0900

    🏭 Change tag method name

--- backend/src/board/views/api/v1/tags.py
+++ backend/src/board/views/api/v1/tags.py
@@ -8,91 +8,93 @@ from board.modules.paginator import Paginator
 from board.modules.time import convert_to_localtime
 
 
-def tags(request, tag=None):
-    if not tag:
-        if request.method == 'GET':
-            tags = Tag.objects.filter(
-                posts__config__hide=False
-            ).annotate(
-                count=Count(
-                    Case(
-                        When(
-                            posts__config__hide=False,
-                            then='posts'
-                        ),
-                    )
+def tag_list(request):
+    if request.method == 'GET':
+        tags = Tag.objects.filter(
+            posts__config__hide=False
+        ).annotate(
+            count=Count(
+                Case(
+                    When(
+                        posts__config__hide=False,
+                        then='posts'
+                    ),
                 )
-            ).order_by('-count')
-
-            tags = Paginator(
-                objects=tags,
-                offset=48,
-                page=request.GET.get('page', 1)
             )
-            return StatusDone({
-                'tags': list(map(lambda tag: {
-                    'name': tag.value,
-                    'count': tag.count,
-                }, tags)),
-                'last_page': tags.paginator.num_pages
-            })
+        ).order_by('-count')
 
-    if tag:
-        if request.method == 'GET':
-            posts = Post.objects.select_related(
-                'config', 'content'
+        tags = Paginator(
+            objects=tags,
+            offset=48,
+            page=request.GET.get('page', 1)
+        )
+        return StatusDone({
+            'tags': list(map(lambda tag: {
+                'name': tag.value,
+                'count': tag.count,
+            }, tags)),
+            'last_page': tags.paginator.num_pages
+        })
+
+    raise Http404
+
+
+def tag_detail(request, name):
+    if request.method == 'GET':
+        posts = Post.objects.select_related(
+            'config', 'content'
+        ).filter(
+            created_date__lte=timezone.now(),
+            config__hide=False,
+            tags__value=name
+        ).annotate(
+            author_username=F('author__username'),
+            author_image=F('author__profile__avatar')
+        ).order_by('-created_date')
+
+        if len(posts) == 0:
+            raise Http404()
+
+        posts = Paginator(
+            objects=posts,
+            offset=24,
+            page=request.GET.get('page', 1)
+        )
+        desc_object = dict()
+        try:
+            article = Post.objects.select_related(
+                'content'
             ).filter(
-                created_date__lte=timezone.now(),
+                url=name,
                 config__hide=False,
-                tags__value=tag
             ).annotate(
                 author_username=F('author__username'),
-                author_image=F('author__profile__avatar')
-            ).order_by('-created_date')
+                author_image=F('author__profile__avatar'),
+            ).first()
 
-            if len(posts) == 0:
-                raise Http404()
+            desc_object = {
+                'url': article.url,
+                'author': article.author_username,
+                'author_image': article.author_image,
+                'description': article.description(50)
+            }
+        except:
+            pass
 
-            posts = Paginator(
-                objects=posts,
-                offset=24,
-                page=request.GET.get('page', 1)
-            )
-            desc_object = dict()
-            try:
-                article = Post.objects.select_related(
-                    'content'
-                ).filter(
-                    url=tag,
-                    config__hide=False,
-                ).annotate(
-                    author_username=F('author__username'),
-                    author_image=F('author__profile__avatar'),
-                ).first()
-
-                desc_object = {
-                    'url': article.url,
-                    'author': article.author_username,
-                    'author_image': article.author_image,
-                    'description': article.description(50)
-                }
-            except:
-                pass
-
-            return StatusDone({
-                'tag': tag,
-                'desc': desc_object,
-                'last_page': posts.paginator.num_pages,
-                'posts': list(map(lambda post: {
-                    'url': post.url,
-                    'title': post.title,
-                    'image': str(post.image),
-                    'description': post.description(),
-                    'read_time': post.read_time,
-                    'created_date': convert_to_localtime(post.created_date).strftime('%Y년 %m월 %d일'),
-                    'author_image': post.author_image,
-                    'author': post.author_username,
-                }, posts))
-            })
+        return StatusDone({
+            'tag': name,
+            'desc': desc_object,
+            'last_page': posts.paginator.num_pages,
+            'posts': list(map(lambda post: {
+                'url': post.url,
+                'title': post.title,
+                'image': str(post.image),
+                'description': post.description(),
+                'read_time': post.read_time,
+                'created_date': convert_to_localtime(post.created_date).strftime('%Y년 %m월 %d일'),
+                'author_image': post.author_image,
+                'author': post.author_username,
+            }, posts))
+        })
 
     raise Http404
