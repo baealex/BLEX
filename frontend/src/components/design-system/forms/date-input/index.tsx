95d809da7f964c112e88bcee9266dce0b8b5bac5
Jino Bae <im@baejino.com>
2023-05-05 13:24:24 +0900

    ✨ Update colors of date picker

--- frontend/src/components/design-system/forms/date-input/index.tsx
+++ frontend/src/components/design-system/forms/date-input/index.tsx
@@ -1,3 +1,7 @@
+import classNames from 'classnames/bind';
+import styles from './DateInput.module.scss';
+const cx = classNames.bind(styles);
+
 import React from 'react';
 import { default as ReactDatePicker } from 'react-datepicker';
 
@@ -19,17 +23,19 @@ export const DateInput = ({
     ...props
 }: DateInputProps) => {
     return (
-        <ReactDatePicker
-            customInput={(
-                <BaseInput
-                    tag="input"
-                    icon={(<i className="far fa-calendar-alt" />)}
-                />
-            )}
-            dateFormat={showTime ? 'yyyy-MM-dd HH:mm' : 'yyyy-MM-dd'}
-            showTimeSelect={showTime}
-            placeholderText={props.placeholder}
-            {...props}
-        />
+        <div className={cx('date-input')}>
+            <ReactDatePicker
+                customInput={(
+                    <BaseInput
+                        tag="input"
+                        icon={(<i className="far fa-calendar-alt" />)}
+                    />
+                )}
+                dateFormat={showTime ? 'yyyy-MM-dd HH:mm' : 'yyyy-MM-dd'}
+                showTimeSelect={showTime}
+                placeholderText={props.placeholder}
+                {...props}
+            />
+        </div>
     );
 };
