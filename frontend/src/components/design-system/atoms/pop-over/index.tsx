3b91efd4afad387aa1e1ee1bd2e38096a609fe10
Jino Bae <im@baejino.com>
2023-05-08 14:52:58 +0900

    ⭐ Add position props on PopOver component

--- frontend/src/components/design-system/atoms/pop-over/index.tsx
+++ frontend/src/components/design-system/atoms/pop-over/index.tsx
@@ -2,27 +2,70 @@ import classNames from 'classnames/bind';
 import styles from './PopOver.module.scss';
 const cn = classNames.bind(styles);
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export interface PopOverProps {
     text: string;
+    position?: 'top' | 'bottom' | 'left' | 'right';
     children: React.ReactNode;
 }
 
-export function PopOver(props: PopOverProps) {
-    const [ hover, setHover ] = useState(false);
+export function PopOver({
+    position = 'left',
+    ...props
+}: PopOverProps) {
+    const popOverRef = useRef<HTMLDivElement>(null);
+    const childrenRef = useRef<HTMLDivElement>(null);
+
+    const [hover, setHover] = useState(false);
+
+    useEffect(() => {
+        if (popOverRef.current && childrenRef.current) {
+            const { current: popOver } = popOverRef;
+            const { current: children } = childrenRef;
+
+            const childrenRect = children.getBoundingClientRect();
+            const popOverRect = popOver.getBoundingClientRect();
+
+            if (position === 'top') {
+                const x = (childrenRect.width - popOverRect.width) / 2;
+                const y = -(popOverRect.height + 16);
+                popOver.style.transform = `translate(${x}px, ${y}px)`;
+            }
+
+            if (position === 'bottom') {
+                const x = (childrenRect.width - popOverRect.width) / 2;
+                const y = childrenRect.height + 16;
+                popOver.style.transform = `translate(${x}px, ${y}px)`;
+            }
+
+            if (position === 'left') {
+                const x = -(popOverRect.width + 16);
+                popOver.style.transform = `translate(${x}px, 0)`;
+            }
+
+            if (position === 'right') {
+                const x = childrenRect.width + 16;
+                popOver.style.transform = `translate(${x}px, 0)`;
+            }
+        }
+    }, [hover, position]);
 
     return (
         <div>
             <div
-                className={cn('popover', { hover })}>
+                ref={popOverRef}
+                className={cn('popover', position, {
+                    hover
+                })}>
                 {props.text}
             </div>
-            <div
+            <span
+                ref={childrenRef}
                 onMouseOver={() => setHover(true)}
                 onMouseLeave={() => setHover(false)}>
                 {props.children}
-            </div>
+            </span>
         </div>
     );
 }
