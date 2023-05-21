1b52394292c536c5cb391d21f8944ffc52f6b26e
Jino Bae <im@baejino.com>
2023-05-21 18:06:24 +0900

    🔧 Fix type error

--- frontend/src/components/system-design/article-editor-page/editor-content/index.tsx
+++ frontend/src/components/system-design/article-editor-page/editor-content/index.tsx
@@ -188,6 +188,8 @@ export function EditorContent(props: EditorContentProps) {
                         props.onChange(easyMDE.value());
                     });
 
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
                     easyMDE.codemirror.on('paste', async (instance, event) => {
                         const item = event.clipboardData?.items[0];
                         if (item && item.type.indexOf('image') !== -1) {
