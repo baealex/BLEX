ab5f0055a2b128ea18a1a928a63820e546016d3d
Jino Bae <im@baejino.com>
2023-06-20 15:55:15 +0900

    🧪 Replenish test case for change username

--- backend/src/board/tests/auth_test.py
+++ backend/src/board/tests/auth_test.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 from django.test import TestCase
 
-from board.models import User, Profile, Config
+from board.models import User, UsernameChangeLog, Profile, Config
 from modules import oauth
 
 
@@ -152,3 +152,46 @@ class AuthTestCase(TestCase):
         self.assertEqual(content['status'], 'DONE')
         self.assertEqual(content['body']['username'], 'test3')
         self.assertEqual(content['body']['isFirstLogin'], False)
+
+    def test_change_username(self):
+        self.client.login(username='test', password='test')
+
+        response = self.client.patch('/v1/sign',
+            'username=test2'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'DONE')
+
+    @patch('modules.markdown.parse_to_html', return_value='<h1>Mocked Text</h1>')
+    def test_change_username_when_have_post(self, mock_service):
+        self.client.login(username='test', password='test')
+
+        response = self.client.post('/v1/posts', {
+            'title': 'Test Post 1',
+            'text_md': '# Test Post',
+            'is_hide': False,
+            'is_advertise': False,
+        })
+
+        response = self.client.patch('/v1/sign',
+            'username=test2'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'DONE')
+
+        # 과거의 유저네임 변경 기록이 남아있는지 확인
+        log = UsernameChangeLog.objects.get(username='test')
+        self.assertEqual(log.user.username, 'test2')
+
+        # 6개월 이내에 재변경이 불가능한지 확인
+        response = self.client.patch('/v1/sign',
+            'username=test3'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'ERROR')
