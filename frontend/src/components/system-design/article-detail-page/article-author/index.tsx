903484c645267a265a72641ed5a2840b76ff794d
Jino Bae <im@baejino.com>
2023-08-07 23:28:42 +0900

    ✨ Update related article style

--- frontend/src/components/system-design/article-detail-page/article-author/index.tsx
+++ frontend/src/components/system-design/article-detail-page/article-author/index.tsx
@@ -18,26 +18,26 @@ export interface ArticleAuthorProps {
 export function ArticleAuthor(props: ArticleAuthorProps) {
     return (
         <div className="pt-5 pb-3">
-            <Flex align="center" justify="between" wrap="wrap">
-                <div className={classNames(styles.image)}>
-                    <Link href="/[author]" as={`/@${props.profile.username}`}>
+            <Flex align="center" justify="between" wrap="wrap" gap={4}>
+                <Flex align="center" wrap="wrap" gap={3}>
+                    <Link className={classNames(styles.image)} href={`/@${props.profile.username}`}>
                         <img src={props.profile.image} />
                     </Link>
-                </div>
-                <div className={classNames(styles.info)}>
-                    <div className={styles.username}>
-                        <Link href={`/@${props.profile.username}`}>
-                            {props.profile.name} (@{props.profile.username})
-                        </Link>
+                    <div className={classNames(styles.info)}>
+                        <div className={styles.username}>
+                            <Link href={`/@${props.profile.username}`}>
+                                {props.profile.name} (@{props.profile.username})
+                            </Link>
+                        </div>
+                        <div className={styles.bio}>
+                            <Link href={`/@${props.profile.username}/about`}>
+                                {props.profile.bio}
+                            </Link>
+                        </div>
                     </div>
-                    <div className={styles.bio}>
-                        <Link href={`/@${props.profile.username}/about`}>
-                            {props.profile.bio}
-                        </Link>
-                    </div>
-                </div>
+                </Flex>
                 <SubscribeButton author={props.profile.username} />
             </Flex>
-        </div>
+        </div >
     );
 }
