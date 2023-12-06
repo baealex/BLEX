cd68c92d47453b215dc51bcd001184832ae440ad
Jino Bae <im@baejino.com>
2023-12-07 00:15:37 +0900

    Get sample db from web storage

--- dev-tools/core.ts
+++ dev-tools/core.ts
@@ -1,5 +1,5 @@
 import { spawn } from 'child_process'
-import { copyFileSync, existsSync } from 'fs'
+import { copyFileSync, existsSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 
 export const SAMPLE_PATH = './dev-tools/sample';
@@ -21,10 +21,10 @@ export function copySampleData() {
             resolve('./dev-tools/sample/frontend/.env'),
             resolve('./frontend/.env')
         )
-
+    
     if (!existsSync(resolve('./backend/src/db.sqlite3')))
-        copyFileSync(
-            resolve('./dev-tools/sample/backend/db.sqlite3'),
-            resolve('./backend/src/db.sqlite3')
-        )
+        fetch('https://www.dropbox.com/scl/fi/frtd7j45fg3nrxrw64oue/db.sqlite3?rlkey=dqbi2f6rrykz2ykq04md3pglg&dl=1')
+            .then(async res => {
+                writeFileSync(resolve('./backend/src/db.sqlite3'), Buffer.from(await res.arrayBuffer()))
+            })
 }
