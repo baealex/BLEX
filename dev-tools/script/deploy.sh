9c9e46b01f091000c163bea65779cccca88a1344
Jino Bae <im@baejino.com>
2024-04-18 13:10:55 +0900

    📦 Reduce docker image size

--- dev-tools/script/deploy.sh
+++ dev-tools/script/deploy.sh
@@ -2,16 +2,16 @@
 
 git pull
 
-docker-compose -p bak -f docker-compose.bak.yml up -d --build
+docker compose -p bak -f docker-compose.bak.yml up -d --build
 
 sleep 15
 
 docker logs blex_backend_1 2> "`date +"%Y%m%d_%H%M%S"`.log"
 
-docker-compose up -d --build
+docker compose up -d --build
 
 sleep 15
 
-docker-compose -p bak -f docker-compose.bak.yml down
+docker compose -p bak -f docker-compose.bak.yml down
 
 docker rmi $(docker images -f "dangling=true" -q)
--- dev-tools/script/dev.sh
+++ dev-tools/script/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -p blex_dev -f docker-compose.dev.yml up "$@"
+docker compose -p blex_dev -f docker-compose.dev.yml up "$@"
 
 docker rmi $(docker images -f "dangling=true" -q)
