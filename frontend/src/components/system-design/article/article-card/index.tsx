d3f7bf764c08f3d7dc9e8ed1b790a01d72d02bd9
Jino Bae <im@baejino.com>
2024-04-20 15:44:32 +0900

    ✨ Update article card style

--- frontend/src/components/system-design/article/article-card/index.tsx
+++ frontend/src/components/system-design/article/article-card/index.tsx
@@ -60,7 +60,56 @@ export function ArticleCard(props: ArticleCardProps) {
     }, [props.description, props.highlight]);
 
     return (
-        <article className={cn(props.className, 'w-100')}>
+        <article className={cn(props.className, 'w-100', 'mt-2')}>
+            <Flex align="center" justify="between">
+                <Flex align="center" gap={1}>
+                    {props.author && (
+                        <Link className="deep-dark" href={`/@${props.author}`}>
+                            <Flex align="center" gap={1}>
+                                <img
+                                    className={cn('author-image')}
+                                    alt={props.author}
+                                    src={getUserImage(props.authorImage || '')}
+                                />
+                                <Text fontSize={3}>
+                                    @{props.author}
+                                </Text>
+                            </Flex>
+                        </Link>
+                    )}
+                    <Text fontSize={2} className="shallow-dark">
+                        ·
+                    </Text>
+                    <Text fontSize={2} className="shallow-dark">
+                        {props.createdDate}
+                    </Text>
+                    <Text fontSize={2} className="shallow-dark">
+                        ·
+                    </Text>
+                    <Text fontSize={2} className="shallow-dark">
+                        {props.readTime}분 분량
+                    </Text>
+                </Flex>
+                <Flex align="center" gap={3}>
+                    {typeof props.countComments === 'number' && (
+                        <button className={cn('action')} onClick={() => router.push(url + '#comments')}>
+                            <Flex align="center" gap={1}>
+                                <i className="far fa-comment" />
+                                <Text>{props.countComments}</Text>
+                            </Flex>
+                        </button>
+                    )}
+                    {typeof props.countLikes === 'number' && (
+                        <button className={cn('action', 'like', { active: props.hasLiked })} onClick={props.onLike}>
+                            <Flex align="center" gap={1}>
+                                {props.hasLiked
+                                    ? <i className="fas fa-heart" />
+                                    : <i className="far fa-heart" />}
+                            </Flex>
+                        </button>
+                    )}
+                </Flex>
+            </Flex>
             <div className={cn('posts')}>
                 <>
                     {props.image && (
@@ -79,19 +128,8 @@ export function ArticleCard(props: ArticleCardProps) {
                             />
                         </Link>
                     )}
-                    <Flex className={cn('content')} direction="column" justify="between">
+                    <Flex className={cn('content')} direction="column">
                         <div className="w-100">
-                            <Flex className={cn('py-2')} align="center" gap={1}>
-                                <Text fontSize={2} className="shallow-dark">
-                                    {props.createdDate}
-                                </Text>
-                                <Text fontSize={2} className="shallow-dark">
-                                    ·
-                                </Text>
-                                <Text fontSize={2} className="shallow-dark">
-                                    {props.readTime}분 분량
-                                </Text>
-                            </Flex>
                             <div className="py-2">
                                 {props.number && (
                                     <div className={cn('number')}>
@@ -124,45 +162,6 @@ export function ArticleCard(props: ArticleCardProps) {
                                     )}
                                 </Link>
                             </div>
-                        </div>
-                        <div className={cn('footer', 'py-2')}>
-                            <Flex justify="between" align="center">
-                                {props.author && (
-                                    <Link className="deep-dark" href={`/@${props.author}`}>
-                                        <Flex align="center" gap={1}>
-                                            <img
-                                                className={cn('author-image')}
-                                                alt={props.author}
-                                                src={getUserImage(props.authorImage || '')}
-                                                width="35"
-                                                height="35"
-                                            />
-                                            <Text fontSize={3}>
-                                                @{props.author}
-                                            </Text>
-                                        </Flex>
-                                    </Link>
-                                )}
-                                <Flex align="center" gap={3}>
-                                    {typeof props.countComments === 'number' && (
-                                        <button onClick={() => router.push(url + '#comments')} className={cn('comment')}>
-                                            <Flex align="center" gap={1}>
-                                                <i className="far fa-comment" />
-                                                <Text>{props.countComments}</Text>
-                                            </Flex>
-                                        </button>
-                                    )}
-                                    {typeof props.countLikes === 'number' && (
-                                        <button onClick={props.onLike} className={cn('like', { active: props.hasLiked })}>
-                                            <Flex align="center" gap={1}>
-                                                {props.hasLiked
-                                                    ? <i className="fas fa-heart" />
-                                                    : <i className="far fa-heart" />}
-                                            </Flex>
-                                        </button>
-                                    )}
-                                </Flex>
-                            </Flex>
                         </div>
                     </Flex>
                 </>
