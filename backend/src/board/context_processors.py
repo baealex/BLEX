8dca6711bb9f786d9ebec0b897847eb97b3c4c7b
Jino Bae <im@baejino.com>
2025-09-02 00:02:07 +0900

    🔧 Fix custom scripts

--- backend/src/board/context_processors.py
+++ backend/src/board/context_processors.py
@@ -9,13 +9,3 @@ def oauth_settings(request):
         'GOOGLE_OAUTH_CLIENT_ID': getattr(settings, 'GOOGLE_OAUTH_CLIENT_ID', ''),
         'GITHUB_OAUTH_CLIENT_ID': getattr(settings, 'GITHUB_OAUTH_CLIENT_ID', ''),
     }
-
-
-def custom_scripts(request):
-    """
-    Add custom scripts to template context
-    """
-    return {
-        'CUSTOM_HEAD_SCRIPTS': getattr(settings, 'CUSTOM_HEAD_SCRIPTS', ''),
-        'CUSTOM_BODY_SCRIPTS': getattr(settings, 'CUSTOM_BODY_SCRIPTS', ''),
-    }
