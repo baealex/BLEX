ad3e1939da5b192b637d7d58ecb3c977e379b944
Jino Bae <im@baejino.com>
2025-09-09 20:38:19 +0900

    ♻️ Hides header dynamically

--- backend/islands/src/scripts/alpine-loader.ts
+++ backend/islands/src/scripts/alpine-loader.ts
@@ -1,5 +1,11 @@
 import Alpine from 'alpinejs';
 
+import headerScroll from './alpine/headerScroll';
+import likeButton from './alpine/likeButton';
+
+Alpine.data('headerScroll', headerScroll);
+Alpine.data('likeButton', likeButton);
+
 document.addEventListener('DOMContentLoaded', () => {
     Alpine.start();
 });
