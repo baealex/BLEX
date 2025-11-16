2174538979aeccdf582bccffa7339646729023f5
Jino Bae <im@baejino.com>
2025-11-16 10:26:47 +0900

    Improve SiteSetting admin with better error handling

--- backend/src/board/admin/site.py
+++ backend/src/board/admin/site.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import HttpResponse
+
 from board.models import SiteSetting
 
 
@@ -23,7 +27,11 @@ class SiteSettingAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         # Only allow one instance (singleton)
-        return not SiteSetting.objects.exists()
+        try:
+            return not SiteSetting.objects.exists()
+        except Exception:
+            # If table doesn't exist, allow creation (after migration)
+            return True
 
     def has_delete_permission(self, request, obj=None):
         # Don't allow deletion
@@ -31,6 +39,21 @@ class SiteSettingAdmin(admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
         # Redirect to the single instance edit page
-        obj = SiteSetting.get_instance()
-        from django.shortcuts import redirect
-        return redirect(f'/admin/board/sitesetting/{obj.pk}/change/')
+        try:
+            # Try to get existing instance first, create only if it doesn't exist
+            obj = SiteSetting.objects.filter(pk=1).first()
+            if obj is None:
+                obj = SiteSetting.objects.create(pk=1, header_script='', footer_script='')
+
+            # Use reverse to get the correct admin URL
+            url = reverse('admin:board_sitesetting_change', args=[obj.pk])
+            return redirect(url)
+        except Exception as e:
+            # If table doesn't exist yet (migrations not run), show error
+            return HttpResponse(
+                f'<h1>SiteSetting 테이블이 생성되지 않았습니다</h1>'
+                f'<p>다음 명령어를 실행하세요:</p>'
+                f'<pre>python manage.py makemigrations\npython manage.py migrate</pre>'
+                f'<p>Error: {str(e)}</p>',
+                status=503
+            )
