8df6f8ec74cd4601cfb6e488fd51d2c99942e1f8
Jino Bae <im@baejino.com>
2025-11-18 08:17:37 +0900

    ✨ Add admin static page builder

--- backend/src/board/admin/site.py
+++ backend/src/board/admin/site.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.http import HttpResponse
+from django import forms
 
-from board.models import SiteSetting
+from board.models import SiteSetting, StaticPage
 
 
 @admin.register(SiteSetting)
@@ -54,3 +55,56 @@ class SiteSettingAdmin(admin.ModelAdmin):
                 f'<p>Error: {str(e)}</p>',
                 status=503
             )
+
+
+class StaticPageAdminForm(forms.ModelForm):
+    """Custom form for StaticPage with rich text editor styling"""
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'rows': 20,
+            'style': 'width: 100%; font-family: monospace; font-size: 14px;'
+        }),
+        help_text='HTML 태그를 사용할 수 있습니다. (예: <h1>, <p>, <div>, <section> 등)'
+    )
+
+    class Meta:
+        model = StaticPage
+        fields = '__all__'
+
+
+@admin.register(StaticPage)
+class StaticPageAdmin(admin.ModelAdmin):
+    """
+    Admin interface for static pages.
+    Allows creating and editing static pages with HTML content.
+    """
+    form = StaticPageAdminForm
+    list_display = ['title', 'slug', 'is_published', 'show_in_footer', 'order', 'updated_date']
+    list_editable = ['is_published', 'show_in_footer', 'order']
+    list_filter = ['is_published', 'show_in_footer']
+    search_fields = ['title', 'slug', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+    fieldsets = (
+        ('기본 정보', {
+            'fields': ('title', 'slug', 'meta_description')
+        }),
+        ('페이지 내용', {
+            'fields': ('content',),
+            'description': 'HTML 코드를 작성하여 페이지를 디자인할 수 있습니다. Tailwind CSS 클래스를 사용할 수 있습니다.'
+        }),
+        ('표시 설정', {
+            'fields': ('is_published', 'show_in_footer', 'order')
+        }),
+        ('메타데이터', {
+            'fields': ('author', 'created_date', 'updated_date'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    readonly_fields = ['created_date', 'updated_date']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.author:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
