aaa5d2780b2d56ef4a1d6c0f487846fa69c138a1
Jino Bae <im@baejino.com>
2026-02-22 23:36:04 +0900

    🐛 Align post detail and editor layout consistency

--- backend/islands/apps/remotes/src/components/remotes/PostEditor/PostEditorWrapper.tsx
+++ backend/islands/apps/remotes/src/components/remotes/PostEditor/PostEditorWrapper.tsx
@@ -6,8 +6,8 @@ interface PostEditorWrapperProps {
 
 const PostEditorWrapper = ({ children }: PostEditorWrapperProps) => {
     return (
-        <div className="pt-8 pb-16">
-            <div className="max-w-4xl w-full mx-auto px-4 md:px-6">
+        <div className="pt-8 pb-16 px-4 md:px-6">
+            <div className="max-w-4xl w-full mx-auto">
                 {children}
             </div>
         </div>
