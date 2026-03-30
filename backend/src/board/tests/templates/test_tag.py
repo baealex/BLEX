498bf0f014b22d9b2cd3b9bd7474ec08bed25fdf
Jino Bae <im@baejino.com>
2026-03-31 01:04:43 +0900

    🧪 Restore template query coverage for main and tags

--- backend/src/board/tests/templates/test_tag.py
+++ backend/src/board/tests/templates/test_tag.py
@@ -1,73 +1,85 @@
 """
-태그 페이지 템플릿 테스트
+Tag page template tests.
 URL: /tags, /tag/<name>
 """
-from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from board.models import Post, PostContent, PostConfig, Tag
+from board.models import Post, PostConfig, PostContent, Tag
 
 
 class TagListPageTestCase(TestCase):
-    """태그 목록 페이지 (/tags) 템플릿 테스트"""
+    """Template tests for the tag list page."""
 
     def setUp(self):
-        """테스트 데이터 설정"""
         self.client = Client()
         self.user = User.objects.create_user(
             username='testuser',
             email='test@example.com',
-            password='testpass123'
+            password='testpass123',
         )
 
-        # 여러 태그와 포스트 생성
         tags_data = ['python', 'django', 'javascript', 'react']
         for tag_name in tags_data:
             tag = Tag.objects.create(value=tag_name)
 
-            # 각 태그에 대한 포스트 생성
             for i in range(3):
                 post = Post.objects.create(
                     title=f'{tag_name} Post {i}',
                     url=f'{tag_name}-post-{i}',
                     author=self.user,
                     created_date=timezone.now(),
-                    published_date=timezone.now()
+                    published_date=timezone.now(),
                 )
                 PostContent.objects.create(
                     post=post,
                     text_md=f'Content about {tag_name}',
-                    text_html=f'<p>Content about {tag_name}</p>'
+                    text_html=f'<p>Content about {tag_name}</p>',
                 )
                 PostConfig.objects.create(
                     post=post,
                     hide=False,
-        
-                    advertise=False
+                    advertise=False,
                 )
                 post.tags.add(tag)
 
     def test_tag_list_page_renders(self):
-        """태그 목록 페이지가 정상적으로 렌더링되는지 테스트"""
         response = self.client.get(reverse('tag_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'board/tags/tag_list.html')
 
+    def test_tag_list_supports_sort_options(self):
+        for sort_type in ['popular', 'name', 'recent']:
+            with self.subTest(sort_type=sort_type):
+                response = self.client.get(reverse('tag_list') + f'?sort={sort_type}')
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.context['page'], 1)
+
+    def test_tag_list_search_filters_results(self):
+        response = self.client.get(reverse('tag_list') + '?q=python')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['tags']), 1)
+        self.assertEqual(response.context['tags'][0]['name'], 'python')
+
+    def test_tag_list_pagination_query_is_supported(self):
+        response = self.client.get(reverse('tag_list') + '?page=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['page'], 1)
+
+
 class TagDetailPageTestCase(TestCase):
-    """태그 상세 페이지 (/tag/<name>) 템플릿 테스트"""
+    """Template tests for the tag detail page."""
 
     def setUp(self):
-        """테스트 데이터 설정"""
         self.client = Client()
         self.user = User.objects.create_user(
             username='testuser',
             email='test@example.com',
-            password='testpass123'
+            password='testpass123',
         )
 
-        # 태그와 포스트 생성
         self.tag = Tag.objects.create(value='python')
 
         for i in range(5):
@@ -76,45 +88,39 @@ class TagDetailPageTestCase(TestCase):
                 url=f'python-post-{i}',
                 author=self.user,
                 created_date=timezone.now(),
-                published_date=timezone.now()
+                published_date=timezone.now(),
             )
             PostContent.objects.create(
                 post=post,
                 text_md=f'Python content {i}',
-                text_html=f'<p>Python content {i}</p>'
+                text_html=f'<p>Python content {i}</p>',
             )
             PostConfig.objects.create(
                 post=post,
                 hide=False,
-    
-                advertise=False
+                advertise=False,
             )
             post.tags.add(self.tag)
 
     def test_tag_detail_page_renders(self):
-        """태그 상세 페이지가 정상적으로 렌더링되는지 테스트"""
         response = self.client.get(reverse('tag_detail', kwargs={'name': 'python'}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'board/tags/tag_detail.html')
 
     def test_tag_detail_page_has_required_context(self):
-        """태그 상세 페이지 컨텍스트에 필수 필드 확인"""
         response = self.client.get(reverse('tag_detail', kwargs={'name': 'python'}))
 
         required_fields = ['tag', 'posts', 'page', 'last_page']
-
         for field in required_fields:
             with self.subTest(field=field):
                 self.assertIn(field, response.context)
 
     def test_tag_detail_page_shows_correct_tag(self):
-        """태그 상세 페이지에서 올바른 태그 표시"""
         response = self.client.get(reverse('tag_detail', kwargs={'name': 'python'}))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['tag'], 'python')
 
     def test_tag_detail_pagination(self):
-        """태그 상세 페이지 페이지네이션 테스트"""
         response = self.client.get(
             reverse('tag_detail', kwargs={'name': 'python'}) + '?page=1'
         )
@@ -122,44 +128,36 @@ class TagDetailPageTestCase(TestCase):
         self.assertEqual(response.context['page'], 1)
 
     def test_tag_with_no_posts_returns_404(self):
-        """포스트가 없는 태그 접근 시 404 반환"""
-        # 포스트가 없는 태그 생성
         Tag.objects.create(value='emptytag')
 
         response = self.client.get(
             reverse('tag_detail', kwargs={'name': 'emptytag'})
         )
-        # tag_detail_view는 포스트가 없으면 404 반환
         self.assertEqual(response.status_code, 404)
 
     def test_nonexistent_tag_returns_404(self):
-        """존재하지 않는 태그 접근 시 404 반환"""
-        # 태그를 생성하지 않고 바로 접근
         response = self.client.get(
             reverse('tag_detail', kwargs={'name': 'nonexistent'})
         )
-        # 포스트가 없으면 404
         self.assertEqual(response.status_code, 404)
 
     def test_tag_detail_does_not_show_hidden_posts(self):
-        """태그 상세 페이지에서 숨겨진 포스트가 표시되지 않는지 테스트"""
         hidden_post = Post.objects.create(
             title='Hidden Python Post',
             url='hidden-python-post',
             author=self.user,
             created_date=timezone.now(),
-            published_date=timezone.now()
+            published_date=timezone.now(),
         )
         PostContent.objects.create(
             post=hidden_post,
             text_md='Hidden content',
-            text_html='<p>Hidden content</p>'
+            text_html='<p>Hidden content</p>',
         )
         PostConfig.objects.create(
             post=hidden_post,
             hide=True,
-
-            advertise=False
+            advertise=False,
         )
         hidden_post.tags.add(self.tag)
 
@@ -170,28 +168,26 @@ class TagDetailPageTestCase(TestCase):
         self.assertNotIn('Hidden Python Post', post_titles)
 
     def test_tag_with_unicode_name(self):
-        """유니코드 태그명 테스트"""
-        unicode_tag = Tag.objects.create(value='파이썬')
+        unicode_tag = Tag.objects.create(value='태그')
 
         post = Post.objects.create(
-            title='Korean Python Post',
-            url='korean-python-post',
+            title='Unicode Tag Post',
+            url='unicode-tag-post',
             author=self.user,
             created_date=timezone.now(),
-            published_date=timezone.now()
+            published_date=timezone.now(),
         )
         PostContent.objects.create(
             post=post,
-            text_md='Korean content',
-            text_html='<p>Korean content</p>'
+            text_md='Unicode content',
+            text_html='<p>Unicode content</p>',
         )
         PostConfig.objects.create(
             post=post,
             hide=False,
-
-            advertise=False
+            advertise=False,
         )
         post.tags.add(unicode_tag)
 
-        response = self.client.get(reverse('tag_detail', kwargs={'name': '파이썬'}))
+        response = self.client.get(reverse('tag_detail', kwargs={'name': '태그'}))
         self.assertEqual(response.status_code, 200)
